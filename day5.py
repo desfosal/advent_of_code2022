3969694479df8eb398e667f1bedd6022df211b38
Alexandre Desfosses Foucault <alexandredesfosses@gmail.com>
2022-12-05 20:18:14 -0500

    part 1 and part 2 working, code cleaned

--- day5.py
+++ day5.py
@@ -5,14 +5,15 @@ lines = file1.readlines()
 number_of_stacks = 9
 
 def initialize_stacks(lines,number_of_stacks):
+
     stacks = [] #list of lists, each sublist is a stack of crates
+    
     for i in range(number_of_stacks):
         stacks.append([])
 
     #starting with the bottom of the pile, fill each stack 
     for line in lines[7::-1]:
-        #print(line)
-        #print(line[1::4])
+        
         crates = line[1::4]     
 
         for index in range(number_of_stacks):
@@ -50,9 +51,7 @@ for line in lines[10:]:
 
     how_many = int(splitted_line[1])
     origin = int(splitted_line[3]) - 1 # python index starts at 0, stack number starts at 1
-    destination = int(splitted_line[5]) - 1       
-
-    
+    destination = int(splitted_line[5]) - 1      
 
     crates = stacks[origin][-how_many:]
     stacks[origin] = stacks[origin][0:-how_many]
@@ -67,4 +66,3 @@ for stack in stacks:
 
 
 
-##### part 2
